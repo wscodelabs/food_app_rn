a52c56711154e41a5d35f9a05c787f3275d4ba99
Ishab Acharya <Ishab@Ishab-Diary.local>
2017-09-04 16:01:33 +0545

    adding splash screen

--- android/app/src/main/java/com/resturant/MainApplication.java
+++ android/app/src/main/java/com/resturant/MainApplication.java
@@ -3,6 +3,7 @@ package com.resturant;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.reactnativecomponent.splashscreen.RCTSplashScreenPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RCTSplashScreenPackage(),
           new VectorIconsPackage(),
           new MainReactPackage(),
           new RCTSplashScreenPackage()
